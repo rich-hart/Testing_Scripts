b5d8c75ccd9241db39f55f0002301275b2c30f7c
rich-hart <rich.hart@rice.edu>
2015-03-20 14:31:19 -0500

    Adding in script templates

---
+++ base_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Python source code - replace this with a description of the code and write the code below this text.
+"""
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+import unittest
+
+class Test(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		raise NotImplementedError()
+
+	@classmethod
+	def tearDownClass(cls):
+		raise NotImplementedError()
+
+	def setUp(self)
+		raise NotImplementedError()
+
+	def tearDown(self)
+		raise NotImplementedError()
+
